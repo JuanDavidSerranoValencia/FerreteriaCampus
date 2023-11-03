063904aa9be4faa4ef47c9236dc6fdee387ddc67
SerranoValenciaJuanDavid <serranovalenciajuandavid949@gmail.com>
2023-11-03 12:34:47 -0500

    feat:Consultas

--- Ferreteria/Clases/Data.cs
+++ Ferreteria/Clases/Data.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Windows.Markup;
@@ -27,7 +30,7 @@ namespace Ferreteria.Clases
                 NombreCliente = "Katia",
                 Email ="Katia123@gmail.com"
             },
-            
+
         };
 
         List<Producto> Productos = new List<Producto>{
@@ -43,8 +46,8 @@ namespace Ferreteria.Clases
                 Id = 2,
                 Nombre = "Gaseosa",
                 PrecioUnit = 2800,
-                Cantidad =40,
-                StockMin = 2,
+                Cantidad =20,
+                StockMin = 10,
                 StockMax = 50
             },
              new Producto(){
@@ -57,7 +60,7 @@ namespace Ferreteria.Clases
             }
         };
 
-        List<Factura> Facturas = new List<Factura> (){
+        List<Factura> Facturas = new List<Factura>(){
             new Factura(){
                 NroFactura =1,
                 Fecha = DateOnly.Parse("01/01/2023"),
@@ -77,55 +80,123 @@ namespace Ferreteria.Clases
                 TotalFactura = 4200
             }
         };
-        
-    /*     public void fecha (){
-            var fecha = DateOnly.Parse("01/12/2023");
 
-            var fecha2 =new DateOnly(2023,05,6) ;
+        public void fecha()
+        {
+            var fecha = DateOnly.Parse("01/12/2023");
+            var fecha2 = new DateOnly(2023, 05, 6);
             System.Console.WriteLine(fecha);
             System.Console.WriteLine(fecha2);
-        } */
+        }
 
-        public void ListaProductos (){
-            var productos = (from p in Productos 
-                                select p).ToList();
-            if (productos.Count()!=0){
+        public void ListaProductos()
+        {
+            var productos = (from p in Productos
+                             select p).ToList();
+            if (productos.Count() != 0)
+            {
                 System.Console.WriteLine("--Lista De Productos--");
-                 productos.ForEach(x => System.Console.WriteLine($"{x.Id}-{x.Nombre}-{x.PrecioUnit}-{x.Cantidad}"));
+                productos.ForEach(x => System.Console.WriteLine($"Id Producto:{x.Id}-Nombre Producto:{x.Nombre}-Precio Unitario:{x.PrecioUnit}-Cantidad En Tienda:{x.Cantidad}-Stock Min:{x.StockMin}-Stock Maximo:{x.StockMax}"));
             }
-            else{
+            else
+            {
                 System.Console.WriteLine("No hay Productos en el sistema");
             }
         }
 
-        public void ProductosAgotarse(){
-            var agotarse = (from p in Productos 
-                                 where p.Cantidad < p.StockMin 
-                                 select p).ToList();
-            if (agotarse.Count()!=0){
+        public void ProductosAgotarse()
+        {
+            var agotarse = (from p in Productos
+                            where p.Cantidad < p.StockMin
+                            select p).ToList();
+            if (agotarse.Count() != 0)
+            {
                 System.Console.WriteLine("--Lista De Productos Agotarse--");
-                 agotarse.ForEach(x => System.Console.WriteLine($"{x.Id}-{x.Nombre}-{x.PrecioUnit}-{x.Cantidad}"));
+                agotarse.ForEach(x => System.Console.WriteLine($"Id Producto:{x.Id}-Nombre Producto:{x.Nombre}-Precio Unitario:{x.PrecioUnit}-Cantidad En tienda:{x.Cantidad}-Stock Minimo:{x.StockMin}"));
             }
-            else{
+            else
+            {
                 System.Console.WriteLine("No hay Productos en el sistema");
             }
         }
 
-        public void FacturasEnero (){
+        public void FacturasEnero()
+        {
             var mes = 01;
             var año = 2023;
-            var FacturasEnero=  (from f in Facturas 
-                                    where f.Fecha.Month == mes  && f.Fecha.Year==año
-                                    select f ).ToList();
-             if (FacturasEnero.Count()!=0){
+            var FacturasEnero = (from f in Facturas
+                                 where f.Fecha.Month == mes && f.Fecha.Year == año
+                                 select f).ToList();
+            if (FacturasEnero.Count() != 0)
+            {
                 System.Console.WriteLine("--Facturas mes de enero--");
-                  FacturasEnero.ForEach(f => System.Console.WriteLine($"{f.NroFactura}-{f.Fecha}-{f.IdCliente}-{f.TotalFactura}"));
+                FacturasEnero.ForEach(f => System.Console.WriteLine($"NroFactura:{f.NroFactura}-Fecha Factura:{f.Fecha}-Id Cliente:{f.IdCliente}-Total Factura:{f.TotalFactura}"));
             }
-            else{
+            else
+            {
                 System.Console.WriteLine("No hay facturas en el mes de enero registradas en el sistema");
-            }                        
-          
+            }
 
+
+        }
+
+        public void ProductosComprar()
+        {
+            var comprar = (from p in Productos
+                           where p.Cantidad < p.StockMin
+                           select p).ToList();
+            if (comprar.Count() != 0)
+            {   
+                System.Console.WriteLine("Productos Para Comprar");
+                foreach (var item in comprar)
+                {
+                    var buy = item.StockMax - item.Cantidad;
+                    System.Console.WriteLine($"Producto Comprar:{item.Nombre}--Cantidad Necesaria a Comprar:{buy}");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("No hay productos para comprar");
+            }
+
+
+        }
+
+        public void ValorInventario(){
+            var productos = (from p in Productos select p ).ToList();
+            var valor =0;
+            System.Console.WriteLine("Valor Inventario");
+            foreach(var item in productos){
+                 valor+= item.Cantidad*item.PrecioUnit;
+                 System.Console.WriteLine($"Producto:{item.Nombre}-Precio Unitario:{item.PrecioUnit}-Cantidad Producto:{item.Cantidad}");
+            }
+            System.Console.WriteLine($"Valor Total Inventario :{valor}");
+
+        }
+
+        public void ProductosFactura(){
+            Console.WriteLine("Ingrese el id de la factura a buscar");
+            int facturaBuscar= Console.Read();
+            var proFactura = (from p in Productos where p.Id ==facturaBuscar select p).ToList();
+            if (proFactura.Count()!=0){
+                
+            }else{
+                System.Console.WriteLine("No se encuentran facturas con el id ingresado.");
+            }
+        }
+
+
+        public void menu()
+        {
+
+            System.Console.WriteLine("\nFerreteria Campus");
+            System.Console.WriteLine("\n1.Listar Productos del inventario" +
+            "\n2.Listar Productos a punto de agotarse" +
+            "\n3.Productos que se deben comprar" +
+            "\n4.Total Factuas mes enero" +
+            "\n5.Productos vendidos en una factura" +
+            "\n6.Valor total de inventario" +
+            "\n7.Salir del programa");
         }
 
     }
--- Ferreteria/Program.cs
+++ Ferreteria/Program.cs
@@ -1,16 +1,69 @@
-﻿using Ferreteria.Clases;
+﻿using System.Reflection.Emit;
+using Ferreteria.Clases;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
         Data data = new Data();
+        var opc = "-1";
+        bool flag = true;
+        while (flag)
+        {  
+            data.menu();
+            Console.WriteLine("\nIngrese la opcion que desea realizar");
+            opc = Console.ReadLine();
+        
+            if (opc == "1")
+            {
+                data.ListaProductos();
+            }
+            else if (opc == "2")
+            {
+             
+                data.ProductosAgotarse();
+            }
+            else if (opc == "3")
+            {
+               
+                data.ProductosComprar();
+            }
+            else if (opc == "4")
+            {
+             
+                data.FacturasEnero();
+            }
+            else if (opc == "5")
+            {
+                data.ProductosFactura();
+                
+            }
+            else if (opc == "6")
+            {
+            
+                data.ValorInventario();
+            }
+            else if (opc == "7")
+            {
+                System.Console.WriteLine("Saliendo del programa ");
+                flag = false;
+            }
+            else
+            {
+                System.Console.WriteLine("la opcion que desea ingresar no es valida");
+            }
 
-        /* data.fecha(); */
-        data.ListaProductos();
-        data.ProductosAgotarse();
-        data.FacturasEnero();
+
+        }
 
     }
 }
+
+
+
+
+
+/*    data.ListaProductos();
+   data.ProductosAgotarse();
+   data.FacturasEnero();
+*/
